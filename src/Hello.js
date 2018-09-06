aff372a9184bc76b661d4a07769d7531dddf2b41
Vaibhav Gaikwad <vaibhav@amdev.in>
2018-09-06 20:11:45 +0530

    #comment project setup and hello world

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './Hello.css'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className= "Hello">
+                <h1> Hello World !! </h1>
+             </div>
+        )
+    }
+}
+
+export default Hello;
