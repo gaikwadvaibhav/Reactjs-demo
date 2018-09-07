7ab3b18665fd23c64d1c86816793fa367b029675
Vaibhav Gaikwad <vaibhav@amdev.in>
2018-09-07 10:40:47 +0530

    #comment states and event handling

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        {/* <header className="App-header"> */}
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <h1 className="App-title">Welcome to React</h1> */}
+        {/* </header> */}
+        {/* <p className="App-intro">
           To get started, edit.
-        </p>
+        </p> */}
         <p>
         <Hello/> 
         </p>
--- src/Hello.js
+++ src/Hello.js
@@ -2,10 +2,20 @@ import React, {Component} from 'react'
 import './Hello.css'
 
 class Hello extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {terms: ''}
+    }
+
+
     render() {
         return (
             <div className= "Hello">
-                <h1> Hello World !! </h1>
+                {/* <h1> Hello World !! </h1> */}
+                <input placeholder='Search' onChange = {(event => this.setState({terms: event.target.value}))} />
+                 State terms : {this.state.terms}
+                
              </div>
         )
     }
